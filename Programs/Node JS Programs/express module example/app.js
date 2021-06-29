4a353e296838d482379d10603f01be896f848763
kaleakash <akash300383@gmail.com>
2021-06-29 13:26:26 +0530

    done

--- Programs/Node JS Programs/express module example/app.js
+++ Programs/Node JS Programs/express module example/app.js
@@ -1,6 +1,12 @@
 let express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
+let name ="Raj Deep"
+
+let template =`
+        <div>${name}</div>
+`
+let employees=[{id:100,name:"Ravi",salary:12000},{id:101,name:"Ramesh",salary:14000}]
 
 // adding as a middleware. may be user-defined or pre-defined module. 
 app.use(bodyParser.urlencoded({extended:true})) // enable body part data and access the property as a key 
@@ -8,6 +14,7 @@ app.use(bodyParser.urlencoded({extended:true})) // enable body part data and acc
 app.get("/",(req,res)=> {
     //console.log(__dirname)
     //res.send(__dirname);
+    //res.send(arrayValue);
     res.sendFile(__dirname+"/index.html");
 })
 
@@ -33,7 +40,7 @@ app.post("/checkLogin",(req,res)=> {
     let user = req.body.user;
     let pass = req.body.pass;
     if(user=="Raj" && pass=="123"){
-        res.send("Successfully Login")
+        res.send("<h1>Successfully Login</h1>")
     }else { 
         res.send("Failure try once again");
     }
