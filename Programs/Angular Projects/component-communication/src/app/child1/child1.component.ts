b7255a14b4c9bbe4c0041bcc86feae8c9e22146d
Akash <akash300383@gmail.com>
2021-07-14 10:03:46 +0530

    commuication

---
+++ Programs/Angular Projects/component-communication/src/app/child1/child1.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child1',
+  templateUrl: './child1.component.html',
+  styleUrls: ['./child1.component.css']
+})
+export class Child1Component implements OnInit {
+
+  @Input()              // this varaible ready to receive the value from parent component variable. 
+  pName:string="";
+
+  age:number=0;
+
+  @Output()
+  em = new EventEmitter<number>();
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  passAge(ageRef:any) {
+    this.age = ageRef.value;
+    this.em.emit(this.age);   // pass the value to parent component
+  }
+}
